5a68064ed946a30ae075d5c065cd0e2dc8616c88
R K <blah@eample.com>
2018-12-03 11:55:50 +0100

    Updates

---
+++ precache-manifest.9a45fdb6420f2305955f5b53014e5463.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b64356e82b66b402b328",
+    "url": "/react-calc-game/static/js/runtime~main.b64356e8.js"
+  },
+  {
+    "revision": "b28b328c449fe5554f1b",
+    "url": "/react-calc-game/static/js/main.b28b328c.chunk.js"
+  },
+  {
+    "revision": "fe65e5159218a8a08e5e",
+    "url": "/react-calc-game/static/js/1.fe65e515.chunk.js"
+  },
+  {
+    "revision": "b28b328c449fe5554f1b",
+    "url": "/react-calc-game/static/css/main.a1f46617.chunk.css"
+  },
+  {
+    "revision": "7eccc5302460a6b97169a2980969a40e",
+    "url": "/react-calc-game/index.html"
+  }
+];
